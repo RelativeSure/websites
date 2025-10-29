2c2a7c7ff0a815f38948397b0223655cb0043ab8
Rasmus <67839982+RelativeSure@users.noreply.github.com>
2025-10-29 14:17:12 +0100

    Remove custom MyLinkGrid, add RepositoryBadges to MDX components - use fumadocs components only

---
+++ websites/fumadocs/src/mdx-components.tsx
@@ -0,0 +1,29 @@
+import defaultMdxComponents from 'fumadocs-ui/mdx';
+import type { MDXComponents } from 'mdx/types';
+import { Accordion, Accordions } from 'fumadocs-ui/components/accordion';
+import { Callout } from 'fumadocs-ui/components/callout';
+import { Card, Cards } from 'fumadocs-ui/components/card';
+import { Tab, Tabs } from 'fumadocs-ui/components/tabs';
+import { File, Folder, Files } from 'fumadocs-ui/components/files';
+import { Step, Steps } from 'fumadocs-ui/components/steps';
+import RepositoryBadges from '@/components/RepositoryBadges';
+
+export function getMDXComponents(components?: MDXComponents): MDXComponents {
+  return {
+    ...defaultMdxComponents,
+    Accordion,
+    Accordions,
+    Callout,
+    Card,
+    Cards,
+    Tab,
+    Tabs,
+    File,
+    Folder,
+    Files,
+    Step,
+    Steps,
+    RepositoryBadges,
+    ...components,
+  };
+}
