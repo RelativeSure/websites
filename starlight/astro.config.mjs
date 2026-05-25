25e7b42bc46d01662441dc3c7a06f5b3572a17c4
RelativeSure <67839982+RelativeSure@users.noreply.github.com>
2026-05-25 14:02:26 +0200

    Fix astro config

--- starlight/astro.config.mjs
+++ starlight/astro.config.mjs
@@ -7,87 +7,91 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rasmusj.dk",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  integrations: [
-    starlight({
-      title: "Rasmus Brøgger Jørgensen",
-      tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
-      lastUpdated: true,
-      logo: {
-        src: "./src/assets/favicon.svg",
-      },
-      editLink: {
-        baseUrl: "https://github.com/RelativeSure/websites/edit/master/starlight/",
-      },
-      components: {
-        ThemeSelect: "./src/components/starlight/ThemeSelect.astro",
-      },
-      expressiveCode: {
-        themes: ["material-theme-ocean", "material-theme-lighter"],
-      },
-      pagefind: {
-        ranking: {
-          pageLength: 0.5,
-          termFrequency: 0.8,
-        },
-      },
-      customCss: [
-        // Path to your Tailwind base styles:
-        // "./src/tailwind.css",
-        // Relative path to your custom CSS file
-        "./src/styles/global.css",
-      ],
-      social: [
-        { icon: "github", label: "GitHub", href: "https://github.com/RelativeSure" },
-        { icon: "mastodon", label: "Mastodon", href: "https://infosec.exchange/@relativesure" },
-        { icon: "x.com", label: "X", href: "https://x.com/RelativeSure" },
-        { icon: "linkedin", label: "LinkedIn", href: "https://www.linkedin.com/in/rasmusbroeggerjoergensen/" },
-      ],
-      sidebar: [
-        {
-          label: "Bookmarks",
-          collapsed: true,
-          autogenerate: {
-            directory: "bookmarks",
-          },
-        },
-        {
-          label: "Good Stuff",
-          autogenerate: {
-            directory: "goodstuff",
-          },
-        },
-        {
-          label: "Linux",
-          autogenerate: {
-            directory: "linux",
-          },
-        },
-        {
-          label: "Projects",
-          autogenerate: {
-            directory: "projects",
-          },
-        },
-        {
-          label: "Website",
-          autogenerate: {
-            directory: "website",
-          },
-        },
-        {
-          label: "Windows",
-          autogenerate: {
-            directory: "windows",
-          },
-        },
-      ],
-    }),
-    mdx(),
-    react(),
-    robotsTxt(),
-  ],
+    site: "https://rasmusj.dk",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [
+        starlight({
+            title: "Rasmus Brøgger Jørgensen",
+            tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 4 },
+            lastUpdated: true,
+            logo: {
+                src: "./src/assets/favicon.svg",
+            },
+            editLink: {
+                baseUrl:
+                    "https://github.com/RelativeSure/websites/edit/master/starlight/",
+            },
+            components: {
+                ThemeSelect: "./src/components/starlight/ThemeSelect.astro",
+            },
+            expressiveCode: {
+                themes: ["material-theme-ocean", "material-theme-lighter"],
+            },
+            pagefind: {
+                ranking: {
+                    pageLength: 0.5,
+                    termFrequency: 0.8,
+                },
+            },
+            customCss: [
+                // Path to your Tailwind base styles:
+                // "./src/tailwind.css",
+                // Relative path to your custom CSS file
+                "./src/styles/global.css",
+            ],
+            social: [
+                {
+                    icon: "github",
+                    label: "GitHub",
+                    href: "https://github.com/RelativeSure",
+                },
+                {
+                    icon: "mastodon",
+                    label: "Mastodon",
+                    href: "https://infosec.exchange/@relativesure",
+                },
+                {
+                    icon: "x.com",
+                    label: "X",
+                    href: "https://x.com/RelativeSure",
+                },
+                {
+                    icon: "linkedin",
+                    label: "LinkedIn",
+                    href: "https://www.linkedin.com/in/rasmusbroeggerjoergensen/",
+                },
+            ],
+            sidebar: [
+                {
+                    label: "Bookmarks",
+                    items: [{ autogenerate: { directory: "bookmarks" } }],
+                },
+                {
+                    label: "Good Stuff",
+                    items: [{ autogenerate: { directory: "goodstuff" } }],
+                },
+                {
+                    label: "Linux",
+                    items: [{ autogenerate: { directory: "linux" } }],
+                },
+                {
+                    label: "Projects",
+                    items: [{ autogenerate: { directory: "projects" } }],
+                },
+                {
+                    label: "Website",
+                    items: [{ autogenerate: { directory: "website" } }],
+                },
+                {
+                    label: "Windows",
+                    items: [{ autogenerate: { directory: "windows" } }],
+                },
+            ],
+        }),
+        mdx(),
+        react(),
+        robotsTxt(),
+    ],
 });
